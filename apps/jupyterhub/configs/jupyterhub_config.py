e34c875713189a8b26cc741f708b0387bedeecc2
JRH-USAF <joel.hansen.10@us.af.mil>
2024-05-01 15:04:45 -0600

    trying to add sudo so the permissions work

--- apps/jupyterhub/configs/jupyterhub_config.py
+++ apps/jupyterhub/configs/jupyterhub_config.py
@@ -66,7 +66,7 @@ c.KubeSpawner.environment = {
 c.KubeSpawner.init_containers = [{
     "name": "fix-permissions",
     "image": "busybox",
-    "command": ["sh", "-c", "chown -v -R 1000:100 /jovyan"],
+    "command": ["sh", "-c", "sudo chown -v -R 1000:100 /jovyan"],
     "volume_mounts": [{
         'mountPath': '/jovyan',
         'name': "home-jovyan-mnt"
