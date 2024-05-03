8428706f4012516ee3e7ab6a6bf9141fb17ecc16
JRH-USAF <joel.hansen.10@us.af.mil>
2024-05-03 15:53:50 -0600

    puttin init container back

--- apps/jupyterhub/configs/jupyterhub_config.py
+++ apps/jupyterhub/configs/jupyterhub_config.py
@@ -66,7 +66,7 @@ c.KubeSpawner.environment = {
 c.KubeSpawner.init_containers = [{
     "name": "fix-permissions",
     "image": "busybox",
-    "command": ["sh", "-c", "find /jovyan \! -user 1000 -exec chown -v 1000:100 {} \;"],
+    "command": ["sh", "-c", "chown -v -R 1000:100 /jovyan"],
     "volume_mounts": [{
         'mountPath': '/jovyan',
         'name': "home-jovyan-mnt"
