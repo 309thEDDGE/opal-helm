be0c0d0da900c4f859c0e2b661c93571f28834ca
Ethan Fison <ethan.fison@us.af.mil>
2024-05-28 15:46:18 -0600

    we should probably remove old packages, eh?

--- apps/nginx/config/copy_conda_channel.sh
+++ apps/nginx/config/copy_conda_channel.sh
@@ -20,6 +20,7 @@ checksum_diff(){
     fi
 }
 
+# ensure copied files have correct permissions
 copy_file(){
     echo $1
     cp -f "$local_channel/$1" "$nginx_channel/$1"
@@ -33,6 +34,11 @@ diff_and_copy(){
     fi
 }
 
+remove_old_packages(){
+    match_string="Only in $nginx_channel/$1"
+    rm -f (diff -q "$local_channel/$1" "$nginx_channel/$1" | awk -v dir="$match_string" '$0 ~ dir {print substr ($3, 1, length($3)-1) "/" $4}'
+}
+
 main() {
     mkdir -p "$nginx_channel/linux-64"
     mkdir -p "$nginx_channel/noarch"
@@ -42,23 +48,23 @@ main() {
     echo "checking for diff in linux-64 repodata"
     if checksum_diff "linux-64/repodata.json"; then
         echo "diff found, copying new packages to linux-64"
+        remove_old_packages linux-64
         for file in linux-64/*; do
             if [[ ! ${file##*/} = "repodata.json" ]]; then
                 diff_and_copy "$file"
             fi
         done
-        #find linux-64 ! -name "repodata.json" -exec bash -c 'diff_and_copy' bash {} \;
         copy_file "linux-64/repodata.json"
     fi
     echo "checking for diff in noarch repodata"
     if checksum_diff "noarch/repodata.json"; then
         echo "diff found, copying new packages to noarch"
+        remove_old_packages noarch
         for file in noarch/*; do
             if [[ ! ${file##*/} = "repodata.json" ]]; then
                 diff_and_copy "$file"
             fi
         done
-        #find noarch ! -name "repodata.json" -exec bash -c 'diff_and_copy' bash {} \;
         copy_file "noarch/repodata.json"
     fi
 }
