e0d042545e5b105e726b68e61f15c0886ecda1ab
Ethan Fison <ethan.fison@us.af.mil>
2024-08-15 15:01:36 -0600

    try setting global ip/port again

--- apps/jupyterhub/configs/jupyterhub_config.py
+++ apps/jupyterhub/configs/jupyterhub_config.py
@@ -43,6 +43,8 @@ c.JupyterHub.last_activity_interval = 60
 c.JupyterHub.tornado_settings = {
     "slow_spawn_timeout:": 0,
 }
+c.JupyterHub.ip = get_name("proxy-public")
+c.JupyterHub.port = get_name_env("proxy-public", "_SERVICE_PORT")
 
 # # Gives spawned containers access to the API of the hub
 #c.Jupyterhub.hub_bind_url = 'http://0.0.0.0:8081'
